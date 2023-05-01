151c16e4a2812e546826c9fd364c88b62701052d
moein qorbani <szmp.info@gmail.com>
2023-05-01 10:30:32 +0330

    ff

--- resource/services_single.php
+++ resource/services_single.php
@@ -9,12 +9,10 @@ if (!defined('ABSPATH')) {
 console("services_single");
 function services_single_script()
 {
-    console("function services_single");
 ?>
     <script>
-        console.log("replace");
         for(var i = 0 ; i <= 3 ; i++){
-            document.getElementById("moein_product").innerHTML.replace("افزودن به سبد خرید" , "جزییات بیشتر");
+            document.getElementById("moein_product").innerHTML = document.getElementById("moein_product").innerHTML.replace("افزودن به سبد خرید" , "جزییات بیشتر");
         }
     </script>
 <?php
