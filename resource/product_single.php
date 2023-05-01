7fb7b13e142536a558a7c228d5988c240afc89b3
moein qorbani <szmp.info@gmail.com>
2023-05-01 14:18:36 +0330

    cc

---
+++ resource/product_single.php
@@ -0,0 +1,26 @@
+
+
+<?php
+
+if (!defined('ABSPATH')) {
+    die('-1');
+}
+
+console("services_single");
+function services_single_script()
+{
+?>
+    <script>
+        function close_popup(){
+            document.getElementById("moein_price").click();
+            document.getElementById("moein_price").click();
+            document.getElementById("moein_price").click();
+            document.getElementById("moein_price").click();
+            document.getElementById("moein_price").click();
+            document.getElementById("moein_price").click();
+        }
+    </script>
+<?php
+}
+
+?>
