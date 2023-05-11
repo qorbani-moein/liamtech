cd3870a539b798b826f61b4d3710a8e31ab198df
moein qorbani <szmp.info@gmail.com>
2023-05-11 16:25:38 +0330

    cc

--- Option_theme.php
+++ Option_theme.php
@@ -18,47 +18,77 @@ if (!defined('ABSPATH')) {
 
 add_action('wp_footer', 'query_page');
 
-function query_page(){
+function query_page()
+{
   include('init.php');
-  
-  if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')   
-    $current_page = "https://";   
-  else  
-    $urcurrent_pagel = "http://";   
+
+  if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+    $current_page = "https://";
+  else
+    $urcurrent_pagel = "http://";
   // Append the host(domain name, ip) to the URL.   
-  $current_page.= $_SERVER['HTTP_HOST'];   
+  $current_page .= $_SERVER['HTTP_HOST'];
 
   // Append the requested resource location to the URL   
-  $current_page.= $_SERVER['REQUEST_URI'];    
+  $current_page .= $_SERVER['REQUEST_URI'];
 
   $page_home = get_site_url() . '/';
   $services_single = $page_home . 'services/';
   $product_single = $page_home . 'product/';
 
-  console( substr($current_page,strlen($current_page) , 1));
-  if(substr($current_page,strlen($current_page)-1 , 1) == "/"){
+  console(substr($current_page, strlen($current_page), 1));
+  if (substr($current_page, strlen($current_page) - 1, 1) == "/") {
     // $page_portfolio = $page_portfolio .  "/";
     // $page_ourservice = $page_ourservice .  "/";
     // $page_about_us = $page_about_us .  "/";
   }
-  
+
   console("start");
-  console("current_page: " . $current_page); 
-  console("page_home: " . $page_home); 
-  console("services_single: " . $services_single); 
+  console("current_page: " . $current_page);
+  console("page_home: " . $page_home);
+  console("services_single: " . $services_single);
   // console("page_portfolio: " . $page_portfolio); 
 
 
   if ($current_page == $page_home) {
-  }if ($current_page == $page_category) {
-  }if ($current_page == $page_archive_portfolio) {
-  }if ($product_single == substr($current_page,0,strlen($product_single))) {
+  }
+  if ($current_page == $page_category) {
+  }
+  if ($current_page == $page_archive_portfolio) {
+  }
+  if ($product_single == substr($current_page, 0, strlen($product_single))) {
     product_single_script();
     replace_txt_add_to_cart();
-  }if ($services_single == substr($current_page,0,strlen($services_single))) {
+  }
+  if ($services_single == substr($current_page, 0, strlen($services_single))) {
     services_single_script();
   }
   //elseif($current_page == substr($current_page,0,strlen($page_shop_en))){
   //     script_page_shop_EnToFa();
   // }
+  header();
+}
+function header()
+{
+?>
+  <script>
+    var all_change = document.querySelectorAll("change-to-show-card").entries;
+    
+    // for(i = 0 ; i <= all_change.length -1 ; i++){
+    //   item_card = all_change[i].innerHtml;
+    // }
+    for(const[i , elem] of all_change.entries()){
+      
+    }
+    
+    var all_change_card_btn = document.querySelectorAll("change-to-show-card add_to_cart_button").entries;
+    var all_change_card_img = document.querySelectorAll("change-to-show-card bdt-wc-product-image a");
+    
+    for(const[i , elem] of all_change.entries()){
+      elem.href = all_change_card_img[i].innerHTML.href;
+    }
+
+  </script>
+
+<?php
 }
--- resource/services_single.php
+++ resource/services_single.php
@@ -1,5 +1,3 @@
-
-
 <?php
 
 if (!defined('ABSPATH')) {
@@ -12,11 +10,12 @@ function services_single_script()
     replace_txt_add_to_cart();
 }
 
-function replace_txt_add_to_cart(){
-    ?>
+function replace_txt_add_to_cart()
+{
+?>
     <script>
-        for(var i = 0 ; i <= 10 ; i++){
-            document.getElementById("moein_product").innerHTML = document.getElementById("moein_product").innerHTML.replace("افزودن به سبد خرید" , "جزییات بیشتر");
+        for (var i = 0; i <= 10; i++) {
+            document.getElementById("moein_product").innerHTML = document.getElementById("moein_product").innerHTML.replace("افزودن به سبد خرید", "جزییات بیشتر");
         }
     </script>
 <?php
